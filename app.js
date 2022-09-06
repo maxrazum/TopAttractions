a207d02ec819f7b045f47cc5a92d47ca92e9f214
maxrazum <75961644+maxrazum@users.noreply.github.com>
2022-09-05 21:01:34 -0300

    added functions to create and show attractions

--- app.js
+++ app.js
@@ -17,8 +17,30 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => {
     res.render('home')
+});
+
+app.get('/attractions', async (req, res) => {
+    const attractions = await Attraction.find({});
+    res.render('attractions/index', {attractions})
+});
+
+app.get('/attractions/new', (req, res) => {
+    res.render('attractions/new')
+});
+
+app.post('/attractions', async (req, res) => {
+    const attraction = new Attraction(req.body.attraction);
+    await attraction.save();
+    res.redirect(`/attractions/${attraction._id}`)
+});
+
+app.get('/attractions/:id', async (req, res) => {
+    const attraction = await Attraction.findById(req.params.id)
+    res.render('attractions/show', {attraction});
 });
 
 app.listen(3000, () => {
