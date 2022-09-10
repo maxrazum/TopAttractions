eafbbddc68333761d0f0546a539889fd98ba9f90
maxrazum <75961644+maxrazum@users.noreply.github.com>
2022-09-10 15:29:29 -0300

    added attraction delete middleware

--- models/attraction.js
+++ models/attraction.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const AttractionSchema = new Schema({
@@ -13,6 +14,16 @@ const AttractionSchema = new Schema({
             ref: 'Review'
         }
     ]
+});
+
+AttractionSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 });
 
 module.exports = mongoose.model('Attraction', AttractionSchema);
