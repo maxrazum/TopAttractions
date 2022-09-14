cc5ae78ed27d769bb36865b2f1366ee8eb93f039
maxrazum <75961644+maxrazum@users.noreply.github.com>
2022-09-14 14:41:26 -0300

    added a constroller to reviews routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,31 +1,15 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 
-const Attraction = require('../models/attraction');
-const Review = require('../models/review');
-
 const catchAsync = require('../utils/catchAsync');
 
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
+const reviews = require('../controllers/reviews');
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-    const attraction = await Attraction.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    attraction.reviews.push(review);
-    await review.save();
-    await attraction.save();
-    req.flash('success', 'Created a new review');
-    res.redirect(`/attractions/${attraction._id}`);
-}));
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Attraction.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success', 'Successfully deleted review');
-    res.redirect(`/attractions/${id}`);
-}));
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
+
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
