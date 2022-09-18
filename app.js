735e2a40f2487a6e52f05805d554746146fe5cfb
maxrazum <75961644+maxrazum@users.noreply.github.com>
2022-09-18 14:17:46 -0300

    errors have been hidden from the flash message

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
 
 const sessionConfig = {
+    name: 'session',
     secret: '!-*-!CHANGEIT!-*-!',
     resave: false,
     saveUninitialized: true,
